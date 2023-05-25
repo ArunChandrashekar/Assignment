37a5888d9d6b42d07fd3a2d35b0fbafe1b348291
arun <carun106@gmail.com>
2023-05-25 11:28:25 +0530

    updating changes, adding new 2 new files

---
+++ script.js
@@ -0,0 +1,5 @@
+function concatName() {
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+      console.log(firstName+ " "+ lastName);
+  }
